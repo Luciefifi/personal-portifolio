72f22d5dedba0518e55d2a5fbf36443673fee0c4
Luciefifi <niyomutonilucie@gmail.com>
2023-10-26 10:43:09 +0300

    added contact page

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -1,10 +1,31 @@
+/* eslint-disable jsx-a11y/img-redundant-alt */
 import React from 'react'
-import ComingSoon from '../component/ComingSoon'
+import Footer from '../component/Footer'
+import Header from '../component/Header'
 
 export default function ContactPage() {
   return (
     <div>
-      <ComingSoon/>
+     <Header/>
+     <div className="contact-cont">
+    <div className="p-infor">
+       {/* <div className="c-image"> */}
+       <img src="/assets/contactphoto.png" alt="image" /> 
+       {/* </div> */}
+    <div className="emp-info">
+      <form action="">
+        <input type="text" placeholder='First Name' /> 
+        <input type="text" placeholder='Last Name'/>
+        <input type="text" placeholder='Email'/>
+       <textarea name="" id="" cols="30" rows="10"/>
+        <button>HIRE ME</button> 
+      </form>
+    </div>
+      </div>
+      
+     </div>
+     <Footer/>
+   
     </div>
   )
 }
